7010abf902a87aef03d1c1709d2a4a6eefb58d7d
floma <flo.marcel@hotmail.com>
2020-04-24 16:19:14 +0200

    Completed IData

--- src/main/java/be/iccbxl/poo/data/IData.java
+++ src/main/java/be/iccbxl/poo/data/IData.java
@@ -1,6 +1,31 @@
 package be.iccbxl.poo.data;
 
+import java.util.List;
+import java.util.UUID;
+
+import be.iccbcl.poo.entities.Book;
+import be.iccbcl.poo.entities.Person;
+
 public interface IData {
 
+	public List<Person> findAllPerson();
+	public List<Person> findByPerson(String property, String value);
 	
+	public List<Book> findAllBook();
+	public List<Book> findByBook(String property, String value);
+	
+	
+	public boolean deletePerson(UUID uuid);
+	public boolean deletePerson(Person p);
+	
+	public boolean deleteBook(UUID uuid);
+	public boolean deleteBook(Book b);
+	
+	public int saveAll();
+	
+	public boolean savePerson(Person p);
+	public boolean saveBook(Book b);
+	
+	public boolean updatePerson(Person p);
+	public boolean updateBook(Book b);
 }
