2278e3dafbfb066f22b717bba8c3912a784dc52a
floma <flo.marcel@hotmail.com>
2020-04-24 16:25:07 +0200

    Completed IOperation

--- src/main/java/be/iccbxl/poo/operation/IOperation.java
+++ src/main/java/be/iccbxl/poo/operation/IOperation.java
@@ -1,5 +1,24 @@
 package be.iccbxl.poo.operation;
 
-public interface IOperation {
+import java.util.List;
 
+import be.iccbcl.poo.entities.Book;
+import be.iccbcl.poo.entities.Person;
+
+public interface IOperation {
+	//Data access
+	public List<Person> getMembres();
+	public List<Book> getBooks();
+	
+	public boolean register(Person P);
+	public boolean register(Book b);
+	
+	public boolean unRegister(Person P);
+	public boolean unRegister(Book b);
+	
+	public boolean update(Person P);
+	public boolean update(Book b);
+	
+	//Logic
+	public int computeRemainingDays(Book b);
 }
